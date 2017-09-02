5513bf37339e350f44bf513797b80d0995cdb827
Alessandro Candolini <ac@spotli.me>
2017-09-02 14:56:16 +0100

    Conversion of codebase to kotlin

---
+++ app/src/main/java/com/alessandrocandolini/currency_converter/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.alessandrocandolini.currency_converter
+
+import android.app.Application
+
+/**
+ * Created by alessandro.candolini on 02/09/2017.
+ */
+
+class BaseApplication : Application()
