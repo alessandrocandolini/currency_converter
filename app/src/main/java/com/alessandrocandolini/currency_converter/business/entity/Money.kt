fd6c0d8d5431b7b38eae71069bc6b1e6476d47e8
Alessandro Candolini <ac@spotli.me>
2017-09-04 09:31:47 +0100

    Money classes

--- app/src/main/java/com/alessandrocandolini/currency_converter/business/entity/Money.kt
+++ app/src/main/java/com/alessandrocandolini/currency_converter/business/entity/Money.kt
@@ -1,6 +1,7 @@
 package com.alessandrocandolini.currency_converter.business.entity
 
 import java.math.BigDecimal
+import java.util.*
 
 /**
  * Created by alessandro.candolini on 03/09/2017.
@@ -17,12 +18,12 @@ typealias MoneyValue = BigDecimal
 /** Typealias for exchange rate, consistent with {@link MoneyValue} */
 typealias ExchangeRateValue = BigDecimal
 
-//sealed class Currency(val currency : String)
+/** Exhaustive list of supported currencies */
+//sealed class Currency(val currencyCode : String)
 //class Dollar() : Currency("USD")
 //class Euro() : Currency("EUR")
 //class Pound() : Currency("GBP")
 
-/** Exhaustive list of supported currencies */
 enum class Currency(val currencyCode: String) {
 
     EUR("EUR"),
@@ -32,19 +33,42 @@ enum class Currency(val currencyCode: String) {
 }
 
 /** Data class for an amount of money and the currency. Naming convention consistent with Java 9 / joda-money */
-data class Money(val amount: MoneyValue, val currency: Currency)
+data class Money(val amount: MoneyValue, val currency: Currency) {
+
+    // for our needs we want the use "compareTo" instead of equals on bigdecimals, so we override the equals and hasCode
+    override fun equals(other: Any?): Boolean{
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as Money
+
+        if (currency != other.currency) return false
+
+        if (amount.compareTo(other.amount) != 0) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = 17
+        val value : Double = amount.toDouble()
+        result = 31 * result + value.hashCode()
+        result = 31 * result + currency.hashCode()
+        return result
+    }
+}
 
 /** Type for representing a function that returns the exchange rate between two currencies */
 // Additional constrains can't be embedded into the weak type system of kotlin, for example
 // ExchangeRate(x,y) = 1/ ExchangeRate(y,x) (assuming no bank costs)
 // transitivity,
+// the rate must be one if newCurrency is equal to the original currency
 // non-zero value (dependent types
 // the fact that this function is total (should provide implementation for all supported currencies)
 // Also, I cant force purity of this function
 typealias ExchangeRate = (originalCurrency: Currency, newCurrency: Currency) -> ExchangeRateValue
 
 /** Type for representing a function that performs the conversion */
-// same limitations as per previous function
 typealias CurrencyConverter = (money: Money, newCurrency: Currency) -> Money
 
 
---
+++ app/src/test/java/com/alessandrocandolini/currency_converter/business/entity/MoneyTest.kt
@@ -0,0 +1,160 @@
+package com.alessandrocandolini.currency_converter.business.entity
+
+import junit.framework.Assert
+import org.junit.Test
+import java.math.BigDecimal
+
+/**
+ * Created by alessandro.candolini on 04/09/2017.
+ */
+class MoneyTest {
+
+    @Test
+    fun test_WhenTheyAreTheSame_MustBeEqual() {
+
+        // given
+        val fakeMoney = Money(BigDecimal.ONE,Currency.EUR)
+
+        // then
+        Assert.assertEquals(fakeMoney,fakeMoney)
+
+    }
+
+    @Test
+    fun test_WhenTheyShareSameCurrencyAndValueAndScala_MustBeEqual() {
+
+        // given
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1),Currency.EUR)
+
+        // then
+        Assert.assertEquals(fakeMoney1,fakeMoney2)
+
+    }
+
+    @Test
+    fun test_WhenTheyShareSameCurrencyAndValueButNotScale_MustBeEqual() {
+
+        // given
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1.00),Currency.EUR)
+
+        // then
+        Assert.assertEquals(fakeMoney1,fakeMoney2)
+    }
+
+    @Test
+    fun test_WhenDifferentTypes_MustBeDifferent() {
+
+        // given
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = "dejuewg"
+
+        // then
+        Assert.assertNotSame(fakeMoney1,fakeMoney2)
+
+    }
+
+    @Test
+    fun test_WhenDifferentValue_MustBeDifferent() {
+
+        // given
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(2),Currency.EUR)
+
+        // then
+        Assert.assertNotSame(fakeMoney1,fakeMoney2)
+    }
+
+    @Test
+    fun test_WhenDifferentCurrency_MustBeDifferent() {
+
+        // given
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1),Currency.USD)
+
+        // then
+        Assert.assertNotSame(fakeMoney1,fakeMoney2)
+    }
+
+    @Test
+    fun test_WhenAddingSameMoneyTwiceToAHashSet_ListSizeMustBeOne() {
+
+        // given
+        val list : MutableCollection<Money> = hashSetOf()
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+
+        // when
+        list.add(fakeMoney1)
+        list.add(fakeMoney1)
+
+        // then
+        Assert.assertEquals(1,list.size)
+    }
+
+    @Test
+    fun test_WhenAddingSameValueTwiceToAHashSet_ListSizeMustBeOne() {
+
+        // given
+        val collection : MutableCollection<Money> = hashSetOf()
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1),Currency.EUR)
+
+        // when
+        collection.add(fakeMoney1)
+        collection.add(fakeMoney2)
+
+        // then
+        Assert.assertEquals(1,collection.size)
+    }
+
+
+
+    @Test
+    fun test_WhenAddingMoneyWithSameValueAndCurrencyToAHashSet_ListSizeMustBeOne() {
+
+        // given
+        val collection : MutableCollection<Money> = hashSetOf()
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1.00),Currency.EUR)
+
+        // when
+        collection.add(fakeMoney1)
+        collection.add(fakeMoney2)
+
+        // then
+        Assert.assertEquals(1,collection.size)
+    }
+
+    @Test
+    fun test_WhenAddingMoneyWithSameValueAndDifferentCurrencyToAHashSet_ListSizeMustBeTwo() {
+
+        // given
+        val collection : MutableCollection<Money> = hashSetOf()
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(1.00),Currency.USD)
+
+        // when
+        collection.add(fakeMoney1)
+        collection.add(fakeMoney2)
+
+        // then
+        Assert.assertEquals(2,collection.size)
+    }
+
+    @Test
+    fun test_WhenAddingMoneyWithDifferentValueAndSameCurrencyToAHashSet_ListSizeMustBeTwo() {
+
+        // given
+        val collection : MutableCollection<Money> = hashSetOf()
+        val fakeMoney1 = Money(BigDecimal.valueOf(1),Currency.EUR)
+        val fakeMoney2 = Money(BigDecimal.valueOf(2),Currency.EUR)
+
+        // when
+        collection.add(fakeMoney1)
+        collection.add(fakeMoney2)
+
+        // then
+        Assert.assertEquals(2,collection.size)
+    }
+}
